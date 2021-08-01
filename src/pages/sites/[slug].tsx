9a03eb1a1899ad2750ac384cdf6d6a88f9bfb990
Dan Austin <dan.austin@hey.com>
2021-08-01 21:03:00 +0100

    refactor: rename query param from id to slug

--- src/pages/sites/[slug].tsx
+++ src/pages/sites/[slug].tsx
@@ -6,21 +6,27 @@ import {
   Heading,
   HStack,
   Stack,
-  ThemingProps,
+  Text,
+  ThemingProps
 } from "@chakra-ui/react";
 import Case from "case";
 import IPTable from "components/IPTable";
+import netboxAPI from "integrations/netboxAPI";
 import { IPTableItem } from "models/IPTableData";
+import { NetboxResponse, Site } from "models/__generated__/netboxAPI";
+import { GetServerSideProps } from "next";
 import { useRouter } from "next/dist/client/router";
-import Error from "next/error";
 import Head from "next/head";
 import React, { useState } from "react";
 import useSWR from "swr";
 
-const SitePage = () => {
-  const { query } = useRouter();
+type SitePageProps = {
+  site: Site
+}
+
+const SitePage = ({site}: SitePageProps) => {
   const { data, error } = useSWR<IPTableItem[]>(
-    `/api/ip-addresses/${query.id}`
+    `/api/ip-addresses/${site.slug}`
   );
   const [tableSize, setTableSize] =
     useState<ThemingProps<"Table">["size"]>("md");
@@ -31,7 +37,7 @@ const SitePage = () => {
   return (
     <>
       <Head>
-        <title>{Case.title(query.id as string)} IP Addresses</title>
+        <title>{site.display} IP Addresses</title>
       </Head>
       <Stack
         spacing={8}
@@ -43,7 +49,7 @@ const SitePage = () => {
           bgGradient="linear(90deg, #00d2ff 0%, #3a47d5 100%)"
           bgClip="text"
         >
-          {Case.title(query.id as string)}
+          {site.display}
         </Heading>
         <HStack spacing={2}>
           <ButtonGroup isAttached size="sm" variant="outline">
@@ -83,11 +89,20 @@ const SitePage = () => {
         </HStack>
       </Stack>
       {data ? (
-        <IPTable
-          mt={8}
-          tableProps={{ size: tableSize, variant: tableVariant }}
-          addresses={data}
-        />
+        data.length > 0 ? (
+          <IPTable
+            mt={8}
+            tableProps={{ size: tableSize, variant: tableVariant }}
+            addresses={data}
+          />
+        ) : (
+          <Center h="90vh" w="100%">
+            <Text size="lg">
+              No results found. Create a VLAN with an IP address range in
+              Netbox.
+            </Text>
+          </Center>
+        )
       ) : (
         <Center h="90vh" w="100%">
           <CircularProgress isIndeterminate />
@@ -97,4 +112,17 @@ const SitePage = () => {
   );
 };
 
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => { 
+  const slug = ctx.query.slug as string
+  const results = (await netboxAPI.get<NetboxResponse<Site[]>>(`/dcim/sites?slug=${slug}`)).data.results
+  if (results.length <= 0) return {notFound: true}
+  console.log(results[0])
+  return {
+    props: {
+      site: results[0]
+    }
+  }
+
+}
 export default SitePage;
