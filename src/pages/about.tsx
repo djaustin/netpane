b245ace1a5a07266632aee34aa4940341f918727
Dan Austin <dan.austin@hey.com>
2021-08-01 21:32:59 +0100

    chore: remove unused pages

--- src/pages/about.tsx
+++
@@ -1,5 +0,0 @@
-import { Heading } from "@chakra-ui/react";
-
-const AboutPage = () => <Heading>About</Heading>
-
-export default AboutPage;
--- src/pages/contact.tsx
+++
@@ -1,5 +0,0 @@
-import { Heading } from "@chakra-ui/react";
-
-const ContactPage = () => <Heading>Contact</Heading>
-
-export default ContactPage;
