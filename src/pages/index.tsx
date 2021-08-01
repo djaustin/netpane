da89602cf2c72010f95f30812e5b213c2b24d2e1
Dan Austin <dan.austin@hey.com>
2021-08-01 21:42:31 +0100

    feat: add search button and on-enter search to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import {
+  Button,
   Center,
   Container,
+  HStack,
   Input,
   InputGroup,
   InputLeftAddon,
@@ -11,17 +13,28 @@ import netboxAPI from "integrations/netboxAPI";
 import { NetboxResponse, Site } from "models/__generated__/netboxAPI";
 import { GetStaticProps } from "next";
 import Head from "next/head";
-import React from "react";
-import { FiSearch } from "react-icons/fi";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import { FiArrowRight, FiSearch } from "react-icons/fi";
 import { Hero } from "../components/Hero";
 import SiteCard from "../components/SiteCard";
 
 type IndexProps = {
-  sites: Site[]
-}
+  sites: Site[];
+};
 
 const Index: React.FC<IndexProps> = ({ sites }) => {
-  const inputBackground = useColorModeValue("gray.200", "gray.800");
+  const router = useRouter();
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  const onKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    const input = event.target as HTMLInputElement;
+    if (event.key === "Enter") searchIfValue()
+  };
+  const searchIfValue = () => {
+    if (searchTerm) router.push(`/search?q=${searchTerm}`);
+  }
+
 
   return (
     <>
@@ -30,15 +43,23 @@ const Index: React.FC<IndexProps> = ({ sites }) => {
       </Head>
       <Container centerContent>
         <Hero title="Netpane" />
-        <InputGroup mt={5} border="2px" shadow="lg" rounded="lg">
-          <InputLeftAddon border="none" borderRight="1px">
-            <FiSearch />
-          </InputLeftAddon>
-          <Input
-            border="none"
-            placeholder='"192.168.1.39", "mysql-db01.qa.example.corp", "App Server"'
-          />
-        </InputGroup>
+        <HStack align="center" mt={5} w="100%">
+          <InputGroup border="2px" shadow="lg" rounded="lg">
+            <InputLeftAddon border="none" borderRight="1px">
+              <FiSearch />
+            </InputLeftAddon>
+            <Input
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              onKeyPress={onKeyPress}
+              border="none"
+              placeholder='"192.168.1.39", "mysql-db01.qa.example.corp", "App Server"'
+            />
+          </InputGroup>
+          <Button onClick={searchIfValue}>
+            <FiArrowRight />
+          </Button>
+        </HStack>
       </Container>
       <SimpleGrid mt={16} rowGap={10} columns={[1, null, null, 2, null, 3]}>
         {sites
@@ -57,6 +78,6 @@ export default Index;
 
 export const getStaticProps: GetStaticProps = async () => {
   const res = await netboxAPI.get<NetboxResponse<Site>>("/dcim/sites");
-  const sites = res.data.results
+  const sites = res.data.results;
   return { props: { sites }, revalidate: 5 };
 };
